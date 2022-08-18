7d827bb4a3d492694f5b71184b80d3ba879f8749
agneborn98 <43062786+agneborn98@users.noreply.github.com>
2022-08-18 10:34:56 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -16,20 +16,6 @@ var pollIntervalDisabled = flag.Bool("poll_interval_disabled", false, "set to tr
 var configFile = flag.String("config", "", "path to the config file for this plugin")
 var err error
 
-// This is designed to be simple; Just change the import above and you're good.
-//
-// However, if you want to do all your config in code, you can like so:
-//
-// // initialize your plugin with any settings you want
-//
-//	myInput := &mypluginname.MyPlugin{
-//		DefaultSettingHere: 3,
-//	}
-//
-// shim := shim.New()
-// shim.AddInput(myInput)
-//
-// // Now the shim.Run() call as below. Note the shim is only intended to run a single plugin.
 func main() {
 	// Parse command line options.
 	flag.Parse()
@@ -37,20 +23,14 @@ func main() {
 		*pollInterval = shim.PollIntervalDisabled
 	}
 
-	// Create the shim. This is what will run your plugins.
 	shimLayer := shim.New()
 
-	// If no config is specified, all imported plugins are loaded.
-	// otherwise, follow what the config asks for.
-	// Check for settings from a config toml file,
-	// (or just use whatever plugins were imported above)
 	err = shimLayer.LoadConfig(configFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Err loading input: %s\n", err)
 		os.Exit(1)
 	}
 
-	// Run a single plugin until stdin closes or we receive a termination signal.
 	if err := shimLayer.Run(*pollInterval); err != nil {
 		fmt.Fprintf(os.Stderr, "Err: %s\n", err)
 		os.Exit(1)
